0bb5ebc40e417409d568ce008b26bd95c4a53425
kaz1m1r <casper.haan@protonmail.com>
2025-08-14 21:46:50 +0200

    first iteration and config files

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ else
     SMALLEST_DISK_SIZE=$NVME1_SIZE_BYTES
 fi
 
-BOOT_START_BYTES=1048576
+BOOT_START_BYTES=1048576 # 1MiB
 BOOT_STOP_BYTES=$((2048 * 1048576 + $BOOT_START_BYTES))
 
 RAID_START_BYTES=$BOOT_STOP_BYTES
@@ -44,7 +44,73 @@ mdadm --create --verbose /dev/md1 --level=1 --raid-devices=2 --metadata=0.90 /de
 
 # Encrypt using LUKS
 cryptsetup luksFormat /dev/md1
-cryptsetup open /dev/md1 cryptroot
+ryptsetup open /dev/md1 cryptroot
 
+# Create lvm
+pvcreate /dev/mapper/cryptroot
+vgcreate vg0 /dev/mapper/cryptroot
+VG_SIZE_BYTES=$(($(sudo vgs --units B --noheadings | awk '{ print substr( $6, 1, length($6)-1 ) }') - 1))
 
-pacman -S --noconfirm - < ./required_packages.txt
+ROOT_LV_SIZE_BYTES="50000000000"
+VAR_LV_SIZE_BYTES="10000000000"
+TMP_LV_SIZE_BYTES="5000000000"
+HOME_LV_SIZE_BYTES=$(( $VG_SIZE_BYTES - $ROOT_LV_SIZE_BYTES - $VAR_LV_SIZE_BYTES - $TMP_LV_SIZE_BYTES))
+
+lvcreate -L "$ROOT_LV_SIZE_BYTES"b vg0 -n root
+lvcreate -L "$VAR_LV_SIZE_BYTES"b vg0 -n var
+lvcreate -L "$TMP_LV_SIZE_BYTES"b vg0 -n tmp
+lvcreate -L "$HOME_LV_SIZE_BYTES"b vg0 -n home
+
+# ext4 filesystem on LVs
+mkfs.fat -F32 /dev/md0
+mkfs.ext4 /dev/vg0/root
+mkfs.ext4 /dev/vg0/home
+mkfs.ext4 /dev/vg0/var
+mkfs.ext4 /dev/vg0/tmp
+
+# mount lvs
+mount /dev/vg0/root /mnt
+mkdir /mnt/boot && mount /dev/md0 /mnt/boot
+mkdir /mnt/home && mount /dev/vg0/home /mnt/home
+mkdir /mnt/var && mount /dev/vg0/var /mnt/var
+mkdir /mnt/tmp && mount /dev/vg0/tmp /mnt/tmp
+
+# Place config files
+rm -rf /mnt/etc/mkinitcpio.conf
+rm -rf /mnt/etc/default/grub
+cp mkinitcpio.conf /mnt/tmp
+cp grub /mnt/tmp
+
+# Edit pacman.conf
+echo "[multilib]" >> /mnt/etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+
+# install arch
+pacstrap /mnt - < ./required_packages
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+# Configure mkinitcpio
+rm -rf /etc/mkinitcpio.conf
+cp /tmp/mkinitcpio.conf /etc/mkinitcpio.conf
+mkinitcpio -P
+
+# configure mdadm
+mdadm --detail --scan >> /etc/mdadm.conf
+
+# Install grub on both ssds
+uuid=$(blkid --match-tag UUID -o value /dev/md1)
+cp /tmp/grub /etc/default/grub
+echo GRUB_CMDLINE_LINUX="cryptdevice=UUID=${uuid}:cryptroot root=/dev/vg0/root" >> /etc/default/grub
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck --removable /dev/"$NVME0"
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck --removable /dev/"$NVME1"
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Configure root user password
+passwd
+
+# Install DE 
+systemctl enable sddm.service
+systemctl enable NetworkManager.service
